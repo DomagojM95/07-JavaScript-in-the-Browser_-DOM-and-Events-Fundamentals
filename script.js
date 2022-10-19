f81815d82860553e26e0356f03a13e063528c178
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-10-19 17:46:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ console.log(document.querySelector('.guess').value);
 
 /////////////////////// handling click events ////////////////////////////////
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,5 +28,27 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'no Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too high';
+      score = score - 1; // score --
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too low';
+      score = score - 1; // score --
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+//////////////// implementing the game logic ///////////////////////////////
